73c7832677d093b77d48d35e69edb3faa4e4c1c9
Shibani Mahapatra <34315569+ShibaniMahapatra@users.noreply.github.com>
2019-09-13 21:18:30 +0530

    Delete rough2.py

--- rough2.py
+++
@@ -1,8 +0,0 @@
-import youtube_dl
-
-
-ydl_opts = {'forcefilename':'True','outtmpl': '/home/shibani/Downloads/rough/filename1'}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    p=ydl.download(['https://www.youtube.com/watch?v=xWOoBJUqlbI'])
-    print(p)
-    print(ydl)
